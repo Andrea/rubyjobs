2c4ead34e8c5116e59f2198c9e88df1279e117b3
Gavin Joyce <gavinjoyce@gmail.com>
2008-12-13 11:35:05 +0000

    added admin::home controller which a successful login now redirects to. cleaned up routes.rb, some old routes were still listed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.login '/login', :controller => 'sessions', :action => 'new'
-  map.register '/register', :controller => 'users', :action => 'create'
-  map.signup '/signup', :controller => 'users', :action => 'new'
-  map.resources :users
 
   map.resource :session
 
-	map.activate '/activate/:activation_code', :controller => 'users', :action => 'activate', :activation_code => nil
-		map.resources :users, :member => { :suspend => :put, :unsuspend => :put, :purge => :delete }
+	map.namespace :admin do |admin|
+		admin.home 'home', :controller => :home
+	end
 
   map.root :controller => "home"
 end
