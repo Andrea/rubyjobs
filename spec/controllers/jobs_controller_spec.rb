1191548b8ca24a3e3a74d9772a79e799dd54f72e
Gavin Joyce <gavinjoyce@gmail.com>
2008-12-15 18:54:24 +0000

    added a factory helper to the specs. added some further job_controller specs

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -1,55 +1,74 @@
 require File.join(File.dirname(__FILE__), '..', "spec_helper" )
+require File.dirname(__FILE__) + '/../factory'
+
+include Factory
 include AuthenticatedTestHelper
 
 describe JobsController do
 	integrate_views
-	
+
 	describe "the index action" do
+		before do
+			@job = Factory.create_job
+			get :index
+		end
 
 		it "should render correctly" do
-			get :index
 			response.should be_success
 		end
-		
+
 		it "should assign a jobs collection" do
-			get :index
 			assigns[:jobs].should_not be_nil
 		end
-		
+
 		describe "when there are jobs to display" do
-			it "should list the most recent jobs"
+			it "should list the most recent jobs" do
+				response.should have_tag("ul#job_list")
+			end
 		end
-		
+
 		describe "when there are no jobs to display" do
-			it "should show a message indicating that there are no jobs"
+			before do
+				Job.destroy_all
+				get :index
+			end
+			
+			it "should show a message indicating that there are no jobs" do
+				response.should have_tag("span#no_jobs")
+			end
+
+			it "should not display a list of jobs" do
+				get :index
+				response.should_not have_tag("ul#job_list")
+			end
 		end
-		
+
 		it "should have more specs"
-		
+
 	end
-	
+
 	describe "the show action" do
 		it "should have specs"
 	end
-	
+
 	describe "the new action" do
 		it "should have specs"
 	end
-	
+
 	describe "the edit action" do
 		it "should have specs"
 	end
-	
+
 	describe "the create action" do
 		it "should have specs"
 	end
-	
+
 	describe "the update action" do
 		it "should have specs"
 	end
-	
+
 	describe "the destroy action" do
 		it "should have specs"
 	end
-	
+
 end
---
+++ spec/factory.rb
@@ -0,0 +1,35 @@
+module Factory
+  def self.create_job(attributes = {})
+    job = Factory.new_job(attributes)
+		job.save!
+		job
+  end
+
+	def self.new_job(attributes = {})
+    default_attributes = {
+   		:company_name 			=> 'ACME Inc.',
+			:company_website 		=> 'www.acme.org',
+			:company_email			=> 'jobs@acme.org',
+			:type_id						=> Factory.create_type.id,
+			:location_id				=> Factory.create_location.id,
+			:title							=> 'Rails developer required for greenfield project',
+			:description				=> 'We are planning on revamping our online store and need someone to build the Rails application',
+			:how_to_apply				=> 'Email jobs@acme.org or call 01-1234567'
+    }
+    Job.new default_attributes.merge(attributes)
+  end
+
+  def self.create_location(attributes = {})
+    default_attributes = {
+      :name => 'Dublin'
+    }
+    Location.create! default_attributes.merge(attributes)
+  end
+
+  def self.create_type(attributes = {})
+    default_attributes = {
+      :name => 'Contract'
+    }
+    Type.create! default_attributes.merge(attributes)
+  end
+end
