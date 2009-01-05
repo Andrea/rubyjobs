ce16880d1567d75fcfe2da3f87f51fdcf0995fa3
Gavin Joyce <gavinjoyce@gmail.com>
2009-01-05 17:08:39 +0000

    added feedburner rss feed for blank searches and the homepage

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,4 +1,9 @@
 module JobsHelper
+	
+	def rss_link
+		param_present?(:search) ? formatted_jobs_url(:rss, rss_link_attributes) : "http://feeds.feedburner.com/rubyjobsireland"
+	end
+	
   def rss_link_attributes
     param_present?(:search) ? {:search => "#{params[:search]}"} : {}
   end
--- app/views/jobs/index.js
+++ app/views/jobs/index.js
@@ -4,8 +4,6 @@ $('#job_list').html('<%= @jobs.any?  ? escape_javascript(render(:partial => @job
   $('#rss_link').attr('href', '<%= formatted_jobs_url(:rss, rss_link_attributes) %>' )
   $('#rss_search').html('RSS feed for \'<%=h params[:search] %>\'')
 <% else %>
-  $('#rss_link').attr('href', '<%= formatted_jobs_url(:rss) %>' )
+  $('#rss_link').attr('href', 'http://feeds.feedburner.com/rubyjobsireland' )
   $('#rss_search').html('RSS feed')
 <% end %>
-
-//	TODO: update the title
