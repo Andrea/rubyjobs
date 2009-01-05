89df230b587726dba1402a9decd955c4f668e9cf
Gavin Joyce <gavinjoyce@gmail.com>
2009-01-05 15:19:27 +0000

    disabled the xml endpoints - these will be re enabled in 0.3

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,7 +5,7 @@ class JobsController < ApplicationController
 
 		respond_to do |format|
 			format.html # index.html.erb
-			format.xml  { render :xml => @jobs }
+			#format.xml  { render :xml => @jobs }
 			format.rss
 			format.js
 		end
@@ -16,7 +16,7 @@ class JobsController < ApplicationController
 
 		respond_to do |format|
 			format.html # show.html.erb
-			format.xml  { render :xml => @job }
+			#format.xml  { render :xml => @job }
 		end
 	end
 
@@ -27,7 +27,7 @@ class JobsController < ApplicationController
 
 		respond_to do |format|
 			format.html # new.html.erb
-			format.xml  { render :xml => @job }
+			#format.xml  { render :xml => @job }
 		end
 	end
 
@@ -46,10 +46,10 @@ class JobsController < ApplicationController
 
 				flash[:notice] = 'Job was successfully created.'
 				format.html { redirect_to :action => 'show', :id => @job.id, :key => @job.key }
-				format.xml  { render :xml => @job, :status => :created, :location => @job }
+				#format.xml  { render :xml => @job, :status => :created, :location => @job }
 			else
 				format.html { render :action => "new" }
-				format.xml  { render :xml => @job.errors, :status => :unprocessable_entity }
+				#format.xml  { render :xml => @job.errors, :status => :unprocessable_entity }
 			end
 		end
 	end
@@ -62,10 +62,10 @@ class JobsController < ApplicationController
 				if @job.update_attributes(params[:job])
 					flash[:notice] = 'Job was successfully updated.'
 					format.html { redirect_to :action => 'show', :id => @job.id, :key => @job.key }
-					format.xml  { head :ok }
+					#format.xml  { head :ok }
 				else
 					format.html { render :action => "edit" }
-					format.xml  { render :xml => @job.errors, :status => :unprocessable_entity }
+					#format.xml  { render :xml => @job.errors, :status => :unprocessable_entity }
 				end
 			end
 		end
@@ -80,7 +80,7 @@ class JobsController < ApplicationController
 
 			respond_to do |format|
 				format.html { redirect_to(jobs_url) }
-				format.xml  { head :ok }
+				#format.xml  { head :ok }
 			end
 		end
 	end
